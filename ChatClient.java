88b56579b1dd251faefa2ce23fcf24d9360539eb
Joan Bonell Ruiz <100761163+JoanBonell@users.noreply.github.com>
2023-04-13 21:34:12 +0200

    First Commit of Paula

---
+++ ChatClient.java
@@ -0,0 +1,63 @@
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+public class ChatClient {
+
+    public static void main(String[] args) throws IOException {
+
+        try {
+            Socket socket = new Socket("localhost", 1234);
+            System.out.println("Connexió establerta.");
+
+            //creem el socket i li passem el nom del host i el port
+            DataInputStream inStream = new DataInputStream(socket.getInputStream());
+            DataOutputStream outStream = new DataOutputStream(socket.getOutputStream());
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+            Thread inputThread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    while (true) {
+                        try {
+                            //aqui li passem el missatge que volem enviar
+                            String message = reader.readLine();
+                            if (!message.isEmpty()) {
+                                outStream.writeUTF(message);
+                                outStream.flush();
+                                if (message.equals("FI")) {
+                                    break;
+                                }
+                            }
+                        } catch (IOException e) {
+                            System.out.println("Error al enviar missatge.");
+                            break;
+                        }
+                    }
+                }
+            });
+            inputThread.start();
+
+            while (true) {
+                //aqui li llegim el missatge que ens envia el servidor
+                String message = inStream.readUTF();
+                if (!message.isEmpty()) {
+                    System.out.println("Servidor: \"" + message + "\"");
+                    if (message.equals("FI")) {
+                        break;
+                    }
+                }
+            }
+            //tanquem el socket
+            inputThread.interrupt();
+            socket.close();
+            System.out.println("Client finalitzat.");
+
+        } catch (IOException e) {
+            System.out.println("Error al connectar el servidor.");
+        }
+    }
+}
---
+++ ChatServer.java
@@ -0,0 +1,74 @@
+// Individual, nom: Paula Silland
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ChatServer {
+
+    public static void main(String[] args) {
+
+        try {
+            ServerSocket serverSocket = new ServerSocket(1234);
+            System.out.println("Servidor iniciant. Esperant conexió...");
+
+            Socket socket = serverSocket.accept();
+            System.out.println("Conexió acceptada.");
+
+            //creem el socket i li passem el nom del host i el port
+            DataInputStream inStream = new DataInputStream(socket.getInputStream());
+            DataOutputStream outStream = new DataOutputStream(socket.getOutputStream());
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+            //creem el thread que s'encarrega de llegir el que escriu l'usuari
+            Thread inputThread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    while(true) {
+                        try {
+                            //aqui li passem el missatge que volem enviar
+                            String message = reader.readLine();
+                            if(!message.isEmpty()) {
+                                outStream.writeUTF(message);
+                                outStream.flush();
+                                if(message.equals("FI")) {
+                                    break;
+                                }
+                            }
+                            //tanquem el socket
+                        } catch (IOException e) {
+                            System.out.println("Error al enviar missatge.");
+                            break;
+                        }
+                    }
+                }
+            });
+            //iniciem el thread
+            inputThread.start();
+
+            while(true) {
+
+                String message = inStream.readUTF();
+                if(!message.isEmpty()) {
+                    System.out.println("Client: \"" + message + "\"");
+                    if(message.equals("FI")) {
+                        break;
+                    }
+                }
+            }
+            //tanquem el socket
+            inputThread.interrupt();
+            serverSocket.close();
+            System.out.println("Servidor finalitzat.");
+
+        } catch (IOException e) {
+            //mostrem el missatge d'error
+            System.out.println("Error al iniciar el servidor: " + e.getMessage());
+        }
+    }
+}
+
